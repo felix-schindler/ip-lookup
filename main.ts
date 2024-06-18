2ac810313816774a9498b77bf38809ae713fd730
Felix <felix-schindler@outlook.com>
2024-06-18 12:10:25 +0800

    Add types; Remove unnecessary code

--- main.ts
+++ main.ts
@@ -1,35 +1,12 @@
+// @deno-types="npm:@types/geoip-lite@1.4.4"
 import { lookup } from "geoip-lite";
 
 Deno.serve((_req: Request, connInfo: Deno.ServeHandlerInfo) => {
 	const ip = connInfo.remoteAddr.hostname;
-	const ipInfo = lookup(ip);
-
-	if (ipInfo !== null && ipInfo !== undefined) {
-		const {
-			country,
-			region,
-			city,
-			ll,
-		} = ipInfo;
-
-		return Response.json({
-			ip,
-			info: {
-				country,
-				region,
-				city,
-				ll,
-			},
-		});
-	}
+	const info = lookup(ip);
 
 	return Response.json({
 		ip,
-		info: {
-			country: "unknown",
-			region: "unknown",
-			city: "unknown",
-			ll: "unknown",
-		},
+		info,
 	});
 });
