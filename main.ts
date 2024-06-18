5be5a0d1833a750cd68a658a0d000e1cc5e966de
Felix <felix-schindler@outlook.com>
2024-06-18 21:57:26 +0800

    Update for fly: actually get the clients ip

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
 // @deno-types="npm:@types/geoip-lite@1.4.4"
 import { lookup } from "geoip-lite";
 
-Deno.serve((_req: Request, connInfo: Deno.ServeHandlerInfo) => {
-	const ip = connInfo.remoteAddr.hostname;
-	const info = lookup(ip);
+Deno.serve((req: Request, connInfo: Deno.ServeHandlerInfo) => {
+	let ip: string;
+
+	if (req.headers.has("Fly-Client-IP")) {
+		ip = req.headers.get("Fly-Client-IP")!;
+	} else {
+		ip = connInfo.remoteAddr.hostname;
+	}
 
 	return Response.json({
 		ip,
-		info,
+		info: lookup(ip),
 	});
 });
