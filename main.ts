1ed216e9e30ebd1682df5eb0befdf7a166d4b210
Felix <felix-schindler@outlook.com>
2024-09-05 13:20:36 +0200

    Get ip from `X-Forwarded-For` header

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 // @deno-types="npm:@types/geoip-lite@1.4.4"
 import { lookup } from "geoip-lite";
 
-Deno.serve((_: Request, connInfo: Deno.ServeHandlerInfo) => {
-	const ip = connInfo.remoteAddr.hostname;
+const HEADER_X_FORWARDED_FOR = "X-Forwarded-For";
+
+Deno.serve((req: Request, connInfo: Deno.ServeHandlerInfo) => {
+	let ip: string;
+
+	if (req.headers.has(HEADER_X_FORWARDED_FOR)) {
+		ip = req.headers.get(HEADER_X_FORWARDED_FOR)!;
+	} else {
+		ip = connInfo.remoteAddr.hostname;
+	}
 
 	return Response.json({
 		ip,
